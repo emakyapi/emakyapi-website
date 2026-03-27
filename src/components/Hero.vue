e2c67f64dc6cc661fb03f584a6f04703b56092c2
emakyapi <167515932+emakyapi@users.noreply.github.com>
2026-03-27 14:20:40 +0300

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -6,6 +6,8 @@
       muted
       loop
       playsinline
+      preload="auto"
+      poster="/emak_hero_poster.jpg"
       class="absolute inset-0 h-full w-full object-cover"
     >
       <source :src="videoPath" type="video/mp4" />
